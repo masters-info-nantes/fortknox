0ecf92474955139a35f7a886f10baa1da80d1211
manon17 <manon.bobin@gmail.com>
2015-11-17 19:22:47 +0100

    End of controler.swt

---
+++ com.mia.banque.application.generated/src/com/mia/banque/ui/controler/swt/CompteControler.java
@@ -0,0 +1,66 @@
+package com.mia.banque.ui.controler.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Group;
+
+import com.mia.banque.ui.controler.ICompteControler;
+import com.mia.banque.ui.view.ICompteView;
+import com.mia.banque.ui.view.swt.CompteView;
+import com.mia.banque.model.ICompte;
+
+public class CompteControler extends Shell implements ICompteControler {
+
+	private CompteView view;
+	private ICompte model;
+
+	public CompteControler(com.mia.banque.model.ICompte model) {
+		this.model = model;
+	}
+
+	public void initView() {	
+		Display display = Display.getDefault();
+		
+		this.view = new CompteView(display, this.model, this);
+		this.view.open();
+		this.view.layout();
+		
+		while (!this.view.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+			
+		}
+	}
+
+	public void afficherOperation() {
+		// Start of user code for afficherOperation
+		if (this.view.OperationList.getSelection().length >0) {
+			String compteSelection = this.view.OperationList.getSelection()[0];
+			for (final com.mia.banque.model.IOperation aOp : this.model.getListOperations()) {
+				if (aOp.toString().equals(compteSelection)) { // TODO gerer un champ id plutï¿½t que utiliser toString()
+					OperationControler opeControler = new OperationControler(aOp);
+					opeControler.initView();
+				}
+			}
+		}		
+		// End of user code for afficherOperation
+	}
+	public void quitter() {
+		// Start of user code for quitter
+		//On veut fermer toutes les fenêtres ouvertes
+		Display.getCurrent().dispose();
+		// End of user code for quitter
+	}
+
+
+}
